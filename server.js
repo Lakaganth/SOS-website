b6b2485435c806994381906b92f6b79833b7041d
lakaganth <lakaganth89@gmail.com>
2020-01-14 11:48:52 -0500

    Build ready -5

--- server.js
+++ server.js
@@ -34,12 +34,19 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema: graphqlSchema,
-    rootValue: graphqlResolver,
-    graphiql: true
+    rootValue: graphqlResolver
+    // graphiql: true
   })
 );
 
 //PORT Config
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
